e9c4ea70c0f5ada1182312fced871e48f2123a91
quintusdev <quintusdev92@gmail.com>
2023-06-01 16:18:03 +0200

    [FEATURES] Aggiunto ciclo For per estrapolare gli oggetti insieme agli array

--- SNACK_2/js/script.js
+++ SNACK_2/js/script.js
@@ -41,14 +41,29 @@ const uppercasePersone = persone.map((elem) => {
 
 console.log(uppercasePersone);
 
+//Ciclo FOR estrarre gli oggetti degli studenti >70
+for (let i = 0; i < uppercasePersone.length; i++) {
+    console.log(uppercasePersone[i]);
+}
+
 //SNACK 2.2: creo una lista di studenti con voti >70
 
 const voti70 = persone.filter((item) => {if(item.grado > '70') return true});
 
 console.log(voti70);
 
+//Ciclo FOR estrarre gli oggetti degli studenti >70
+for (let i = 0; i < voti70.length; i++) {
+    console.log(voti70[i]);
+}
+
 //SNACK 2.3: creo una lista di studenti con voti >70 e quelli che hanno un ID > 120
 
 const id120 = persone.filter((item) => {if(item.grado > '70' && item.id > '120') return true});
 
 console.log(id120);
+
+//Ciclo FOR estrarre gli oggetti degli studenti >70 e con ID > 120
+for (let i = 0; i < id120.length; i++) {
+    console.log(id120[i]);
+}

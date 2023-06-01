17df6d992110c11b0a9c2f1bf65532ec1faf181a
quintusdev <quintusdev92@gmail.com>
2023-06-01 14:37:23 +0200

    [FEATURES] SNACK 1 COMPLETATO

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-//creo l'array 
+//creo l'array degli invitati
 
-const  = ['', '', '', '', '', ''];
+const invitati = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
-const  = [
-{ 'name': '',
-'role': '',
+//inserisco nella costante segnaposto creando un nuovo array dove per ogni invitato metto il tavolo, il nome e il numero del posto
+const segnaposto = invitati.map((persona, index) => {
+    
+    let persone = 
+    { 'tavolo': 'Tavolo VIP',
+        'nome': persona,
+        'value': index + 1,
+    }
+    return persone;
+});
 
-},
+//creo un FOR per ciclare l'array generale e visualizzarlo in console.
 
-];
+for (let i = 0; i < segnaposto.length; i++){
+    let assegnazione = segnaposto[i];
+    console.log(assegnazione.tavolo);
+    console.log(assegnazione.nome);
+    console.log(assegnazione.value);
+}
 
